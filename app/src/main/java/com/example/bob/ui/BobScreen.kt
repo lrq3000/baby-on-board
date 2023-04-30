50a318f8d64ded401b0b1feb8aa6854400bf88d4
Baudouin Coupey <baudouin.coupey@ik.me>
2023-04-30 23:44:15 +0200

    Clean UI with changing bottom bar with Navigation Bar component

--- app/src/main/java/com/example/bob/ui/BobScreen.kt
+++ app/src/main/java/com/example/bob/ui/BobScreen.kt
@@ -4,20 +4,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LinearScale
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.rounded.CalendarMonth
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Insights
 import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.Timer
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,9 +32,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.bob.HomeScreen
 import com.example.bob.InformationScreen
@@ -47,11 +49,13 @@ import com.example.bob.ui.viewModel.BobViewModel
 enum class BobScreen() {
     Home,
     Fruits,
-    Chart,
+
+    //    Chart,
     Calendar,
     CalendarList,
     Informations,
-    AddNote
+    AddNote,
+    Contraction
 }
 
 @Composable
@@ -82,54 +86,48 @@ fun BobTopAppBar(navController: NavController, modifier: Modifier = Modifier) {
 
 @Composable
 fun BobBottomAppBar(navController: NavController, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(72.dp)
-            .background(color = MaterialTheme.colorScheme.surfaceVariant),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        IconButton(onClick = { navController.navigate(BobScreen.Home.name) }) {
-            Icon(
-                imageVector = Icons.Rounded.Home,
-                contentDescription = "home",
-                tint = Color.White,
-                modifier = modifier.size(76.dp)
-            )
-        }
-        IconButton(onClick = { navController.navigate(BobScreen.Fruits.name) }) {
-            Icon(
-                imageVector = Icons.Filled.LinearScale,
-                contentDescription = "Linear Scale",
-                tint = Color.White,
-                modifier = modifier.size(76.dp)
-            )
-        }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(
-                imageVector = Icons.Rounded.Insights,
-                contentDescription = "Charts",
-                tint = Color.White,
-                modifier = modifier.size(76.dp)
-            )
-        }
-        IconButton(onClick = { /*TODO*/ }) {
+    NavigationBar() {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        NavigationBarItem(
+            selected = currentDestination?.hierarchy?.any { it.route == "Home" } == true,
+            onClick = { navController.navigate(BobScreen.Home.name) },
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.Home,
+                    contentDescription = "Home"
+                )
+            })
+        NavigationBarItem(
+            selected = currentDestination?.hierarchy?.any { it.route == "Fruits" } == true,
+            onClick = { navController.navigate(BobScreen.Fruits.name) },
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.Insights,
+                    contentDescription = "Fruits"
+                )
+            })
+        NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
             Icon(
                 imageVector = Icons.Rounded.CalendarMonth,
-                contentDescription = "Fruit",
-                tint = Color.White,
-                modifier = modifier.size(76.dp)
+                contentDescription = "Calendar"
             )
-        }
-        IconButton(onClick = { navController.navigate(BobScreen.CalendarList.name) }) {
+        })
+        NavigationBarItem(
+            selected = currentDestination?.hierarchy?.any { it.route == "CalendarList" } == true,
+            onClick = { navController.navigate(BobScreen.CalendarList.name) },
+            icon = {
+                Icon(
+                    imageVector = Icons.Rounded.List,
+                    contentDescription = "Notes"
+                )
+            })
+        NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
             Icon(
-                imageVector = Icons.Rounded.List,
-                contentDescription = "List",
-                tint = Color.White,
-                modifier = modifier.size(76.dp)
+                imageVector = Icons.Rounded.Timer,
+                contentDescription = "Contraction"
             )
-        }
+        })
     }
 }
 
@@ -170,7 +168,7 @@ fun BobApp(
             }
 
             composable(route = BobScreen.AddNote.name) {
-                AddNoteScreen(onSaveButtonClicked = {navController.navigate(BobScreen.CalendarList.name)})
+                AddNoteScreen(onSaveButtonClicked = { navController.navigate(BobScreen.CalendarList.name) })
             }
         }
     }

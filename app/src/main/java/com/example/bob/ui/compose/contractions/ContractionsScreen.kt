34f05608f1ac48473e3f683e6747eb261fa40e39
Baudouin Coupey <baudouin.coupey@ik.me>
2023-05-13 13:33:42 +0200

    Finished UI of ContractionScreen

--- app/src/main/java/com/example/bob/ui/compose/contractions/ContractionsScreen.kt
+++ app/src/main/java/com/example/bob/ui/compose/contractions/ContractionsScreen.kt
@@ -1,6 +1,6 @@
 package com.example.bob.ui.compose.contractions
 
-import android.util.Log
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,7 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,6 +26,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.alpha
@@ -48,6 +57,10 @@ fun formatedHour(date: LocalDateTime): String {
 
 fun contractionDuration(contraction: Contraction): String {
     val seconds = ChronoUnit.SECONDS.between(contraction.startTime, contraction.endTime)
+    return secondToTime(seconds)
+}
+
+fun secondToTime(seconds: Long): String {
     val min = ((seconds / 60) % 60)
     val sec = seconds % 60
     return if (sec < 10) {
@@ -57,29 +70,63 @@ fun contractionDuration(contraction: Contraction): String {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 @Preview(showBackground = true)
-fun ContractionScreen() {
-    val sortedContraction = contractions.sortedByDescending { it.startTime }
+fun ContractionScreen(onAddButtonClicked: () -> Unit = {}) {
+    Scaffold(floatingActionButton = {
+        LargeFloatingActionButton(
+            onClick = { onAddButtonClicked() },
+            shape = CircleShape,
+        ) {
+            Icon(imageVector = Icons.Filled.Add, contentDescription = "Add contraction")
+        }
+    }, floatingActionButtonPosition = FabPosition.Center) {
+        ContractionBody(
+            sortedContraction = contractions.sortedByDescending { it.startTime }
+        )
+    }
+}
+
+@Composable
+fun ContractionBody(sortedContraction: List<Contraction>) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 8.dp)
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+        ) {
+            Row(modifier = Modifier.fillMaxWidth(.50f), horizontalArrangement = Arrangement.End) {
+                Text(text = "Début")
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(text = "Durée")
+                Spacer(modifier = Modifier.width(24.dp))
+            }
+            Spacer(modifier = Modifier.fillMaxWidth(.14f))
+            Text(
+                text = "Fréquence",
+                color = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
         sortedContraction.forEachIndexed { index, contraction ->
             var timeBetwinContractionDuration: Long? = null
             if (index > 0) {
                 timeBetwinContractionDuration =
                     ChronoUnit.SECONDS.between(
                         contraction.startTime,
-                        sortedContraction[index - 1 ].endTime
+                        sortedContraction[index - 1].startTime
                     )
             }
-            Log.e("time", timeBetwinContractionDuration.toString())
-            ContractionRow(contraction)
-            if (index != sortedContraction.lastIndex) {
-                TimeBetwinContractions(index.toLong())
+            if (index != 0) {
+                TimeBetwinContractions(
+                    timeBetwinContractionDuration = timeBetwinContractionDuration!!
+                )
             }
+            ContractionRow(contraction)
         }
     }
 }
@@ -88,7 +135,6 @@ fun ContractionScreen() {
 fun ContractionRow(
     contraction: Contraction,
 ) {
-    val brush = Brush.horizontalGradient(listOf(Color.Red, Color.Blue))
     Row(
         modifier = Modifier.fillMaxWidth(.5f),
         verticalAlignment = Alignment.CenterVertically,
@@ -96,19 +142,22 @@ fun ContractionRow(
     ) {
         Text(text = formatedHour(contraction.startTime))
         Spacer(modifier = Modifier.width(16.dp))
-        Text(text = contractionDuration(contraction).toString())
+        Text(
+            text = contractionDuration(contraction),
+            fontStyle = FontStyle.Italic,
+        )
         Spacer(modifier = Modifier.width(24.dp))
         val color1 = android.graphics.Color.argb(
-            MaterialTheme.colorScheme.primary.toArgb().alpha,
-            MaterialTheme.colorScheme.primary.toArgb().red,
-            MaterialTheme.colorScheme.primary.toArgb().green,
-            MaterialTheme.colorScheme.primary.toArgb().blue
-        )
-        val color2 = android.graphics.Color.argb(
             MaterialTheme.colorScheme.secondary.toArgb().alpha,
             MaterialTheme.colorScheme.secondary.toArgb().red,
             MaterialTheme.colorScheme.secondary.toArgb().green,
             MaterialTheme.colorScheme.secondary.toArgb().blue
+        )
+        val color2 = android.graphics.Color.argb(
+            MaterialTheme.colorScheme.primary.toArgb().alpha,
+            MaterialTheme.colorScheme.primary.toArgb().red,
+            MaterialTheme.colorScheme.primary.toArgb().green,
+            MaterialTheme.colorScheme.primary.toArgb().blue
         )
         Text(text = (contractions.indexOf(contraction) + 1).toString(),
             color = MaterialTheme.colorScheme.onPrimary,
@@ -118,25 +167,30 @@ fun ContractionRow(
                         listOf(
                             Color(color1), Color(color2)
                         ),
-                        end = Offset(x = 0f, 100f)
+                        end = Offset(x = 0f, 50f)
                     )
                     onDrawBehind {
                         drawCircle(
-                            brush = brush, radius = 40f,
+                            brush = brush, radius = 45f,
                         )
                     }
-                })
+                }
+        )
     }
 }
 
 @Composable
-fun TimeBetwinContractions(timeBetwinContractionDuration: Long?) {
+fun TimeBetwinContractions(timeBetwinContractionDuration: Long) {
     Row(
         Modifier
             .fillMaxWidth()
             .padding(vertical = 4.dp)
     ) {
-        Spacer(modifier = Modifier.fillMaxWidth(.55f))
-        Text(timeBetwinContractionDuration.toString())
+        Spacer(modifier = Modifier.fillMaxWidth(.57f))
+        Text(
+            secondToTime(timeBetwinContractionDuration),
+            color = MaterialTheme.colorScheme.secondary,
+            fontWeight = FontWeight.Bold,
+        )
     }
 }
